31ec39b5e5dafbe5514c1b59d5f7350e96a069aa
oghene-ella <oghenekaro57@gmail.com>
2023-09-07 10:49:41 +0100

    worked on the day & time functionality

---
+++ src/index.js
@@ -0,0 +1,52 @@
+// call the date function
+const date = new Date();
+
+// days of the week function
+const daysFunction = () => {
+	// declare variables
+	const day = document.querySelector(".day");
+
+	const dayOfTheWeek = date.getDay();
+
+	// if statement
+	if (dayOfTheWeek === 0) {
+		day.textContent = "Sunday";
+	} else if (dayOfTheWeek === 1) {
+		day.textContent = "Monday";
+	} else if (dayOfTheWeek === 2) {
+		date.textContent = "Tuesday";
+	} else if (dayOfTheWeek === 3) {
+		date.textContent = "Wednesday";
+	} else if (dayOfTheWeek === 4) {
+		day.textContent = "Thursday";
+	} else if (dayOfTheWeek === 5) {
+		date.textContent = "Friday";
+	} else if (dayOfTheWeek === 6) {
+		date.textContent = "Saturday";
+	} else {
+		date.textContent = "Not Found";
+	}
+};
+daysFunction();
+
+const timeFunction = () => {
+	const time = document.querySelector(".time_utc");
+	let getHour = date.getHours();
+	let getMin = date.getMinutes();
+	let getSec = date.getSeconds();
+	let am_pm;
+
+	// if statement
+	if (getMin > 0 && getMin <= 9) {
+		getMin = `0${getMin}`;
+	}
+	if (getSec > 0 && getSec <= 9) {
+		getSec = `0${getSec}`;
+	}
+
+	// join hour, minute, and second
+	const currentTime = `${getHour}:${getMin}:${getSec}`;
+
+	time.textContent = currentTime;
+};
+timeFunction();
